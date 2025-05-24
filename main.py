63ddcd9902b137bea5897f65b69c014dee2a54ce
alex_barmen <www.lad282@gmail.com>
2025-05-24 11:21:21 +0300

    feat: ...

--- main.py
+++ main.py
@@ -16,5 +16,68 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    """Корневая страница"""
-    return templates.TemplateResponse("index.html", {"request": request})
+    """Главная страница"""
+    with get_db_connection() as conn:
+        agency_sql = conn.execute("SELECT * FROM Agency").fetchall()
+
+        agency = [dict(agency) for agency in agency_sql]
+
+    context = {
+        'request': request,
+        'agencies': agency,
+    }
+    return templates.TemplateResponse("index.html", context)
+
+
+@app.get("/clients", response_class=HTMLResponse)
+async def systems(request: Request):
+    """Получение списка всех систем"""
+    with get_db_connection() as conn:
+        clients_sql = conn.execute("SELECT * FROM Client").fetchall()
+
+        clients = [dict(client) for client in clients_sql]
+
+    context = {
+        'request': request,
+        'clients': clients,
+    }
+
+    return templates.TemplateResponse("clients.html", context)
+
+
+@app.post("/clients", response_class=RedirectResponse)
+async def create_client(
+        fio: str = Form(...),
+        money: float = Form(...),
+        town: int = Form(...),
+        agency: str = Form(...),
+    ):
+
+    with get_db_connection() as conn:
+        conn.execute(
+            '''
+            insert into systems (name, diameter, age, galaxy)
+            values (?, ?, ?, ?)
+            ''',
+            (fio, money, town, agency)
+        )
+
+        conn.commit()
+
+    return RedirectResponse('/clients', status_code=303)
+
+
+@app.get("/estates", response_class=HTMLResponse)
+async def systems(request: Request):
+    """Получение списка всех систем"""
+    with get_db_connection() as conn:
+        estates_sql = conn.execute("SELECT * FROM Estate").fetchall()
+
+        estates = [dict(estate) for estate in estates_sql]
+
+    context = {
+        'request': request,
+        'estates': estates,
+    }
+
+    return templates.TemplateResponse("estates.html", context)
